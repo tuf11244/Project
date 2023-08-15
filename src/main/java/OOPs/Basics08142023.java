b6184dfb3683de60a49102be33983f7da148f9c4
@parthdesai <parth@DESKTOP-3CV400I>
2023-08-15 00:29:52 -0400

    OOP's Basics 08/14/2023

---
+++ src/main/java/OOPs/Basics08142023.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package OOPs;
+
+/**
+ *
+ * @author parth
+ */
+public class Basics08142023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        //Human parth = new Human();
+       // parth.greeting();
+        Student jigar = new Student();
+        Student varsha = new Student("Varsha Desai",8,95);
+        System.out.println(varsha.marks);
+      //  System.out.println(jigar.marks);
+        // TODO code application logic here
+    }
+}
+
+class Human{
+    
+    public void greeting(){
+        System.out.println("Hello human ...how are you");
+    }
+}
+
+class Student{
+   String name;
+   int rollnumber;
+   int marks;
+   
+   Student(){
+       this.name = "Jigar Desai";
+       this.rollnumber = 7;
+       this.marks = 88;
+       
+   }
+   
+   Student(String name, int rollnumber, int marks){
+       this.name = name;
+       this.rollnumber = rollnumber;
+       this.marks = marks;
+   }
+    
+   
+}
