1da211a482fb71760d53a1935cb041ae2ef0c7e2
@parthdesai <parth@DESKTOP-3CV400I>
2023-09-15 19:44:28 -0400

    Stack and Queue Questions

--- src/main/java/StacksandQueuesProblems/ValidParentheses.java
+++ src/main/java/StacksandQueuesProblems/ValidParentheses.java
@@ -8,6 +8,7 @@ import java.util.Stack;
  *Date: 09/14/2023
  * https://leetcode.com/problems/valid-parentheses/description/
  * https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+ * https://leetcode.com/problems/minimum-insertions-to-balance-a-parentheses-string/
  * @author parth
  */
 
@@ -16,8 +17,10 @@ public class ValidParentheses {
 
     public static void main(String[] args) {
        // String expression = "(){}[]";
-        System.out.println(validParentheses("(){}[]"));
-        System.out.println(minAddToMakeValid("{{"));
+       // System.out.println(validParentheses("(){}[]"));
+        //System.out.println(minAddToMakeValid("{{"));
+        System.out.println(minInsertions("())"));
+        
     }
 
     public static boolean validParentheses(String expression) {
@@ -49,6 +52,7 @@ public class ValidParentheses {
 
         return stack.isEmpty(); // Stack should be empty if all brackets are matched.
     }
+//Method 2 for Question 2     
     public static int minAddToMakeValid(String expression){
         int count = 0;
         Stack<Character> stack = new Stack<>();
@@ -78,6 +82,58 @@ public class ValidParentheses {
         }
         return count + stack.size();
        
+}
+    //Method 3 for question 3 
+    public static  int minInsertions(String expression){
+        int count = 0; 
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < expression.length();i++){
+            char currentChar = expression.charAt(i);
+            if(currentChar == '('){
+                if(stack.empty()){
+                    stack.push(currentChar);
+                }else{
+                    if(stack.peek()== '('){
+                        stack.push(currentChar);
+                    }else{
+                        count = count + 1;
+                        stack.pop();
+                        stack.pop();
+                        stack.push(currentChar);
+                    }
+                }
+            }else{
+                if(stack.empty()){
+                    count = count + 1;
+                    stack.push('(');
+                    stack.push(currentChar);
+                }else{
+                    if(stack.peek()== '('){
+                        stack.push(currentChar);
+                    }else{
+                        stack.pop();
+                        stack.pop();
+                    }
+                }
+                
+            }     
+                
+   }
+        if(stack.empty()){
+            return count;
+        }else{
+            while(!stack.empty()){
+                char top = stack.pop();
+                if(top == '('){
+                    count = count + 2;
+                }else{
+                    count = count + 1;
+                    stack.pop();
+                }
+            }
+        }
+        return count;
+   
 }
 }
 
