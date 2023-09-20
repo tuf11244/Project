e1f61f46d604afb88f8e0ebefb2b8e0493bbd48d
@parthdesai <parth@DESKTOP-3CV400I>
2023-09-19 23:52:04 -0400

    Binary Trees

---
+++ src/main/java/Trees/BST.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package Trees;
+
+/**
+ *Date:09/19/2023
+ * @author parth
+ */
+public class BST {
+    private Node root;
+    
+    public BST(){     
+    }
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+    public boolean isEmpty(){
+        return root == null;
+    }
+    //inserting elements 
+    public void insert(int value){
+        root = insert(root,value);
+    }
+    private Node insert(Node node, int value){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+        if(value < node.value){
+           node.left = insert(node.left,value);
+        }
+        if(value > node.value){
+            node.right = insert(node.right,value);
+        }
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
+        return node;       
+    }
+    public void display(){
+        display(this.root,"Root value is :");
+    }
+    public void display(Node node, String details){
+        if(node == null){
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left,"The left child of " + node.value + ":");
+        display(node.right,"The right child of " + node.value + ":");
+    }
+    public void populate(int[] arr){
+        for(int i = 0; i < arr.length;i++){
+            this.insert(arr[i]);
+        }
+    }
+    public void populateSorted(int[] arr){
+        populateSorted(arr,0,arr.length);
+    }
+    private void populateSorted(int[] arr, int start, int end){
+        if(start >= end){
+            return;
+        }
+        int middle = (start + end)/2;
+        this.insert(arr[middle]);
+        populateSorted(arr,start,middle);
+        populateSorted(arr,middle+1,end);
+    }
+    public boolean balanced(){
+        return balanced(root);
+    }
+    private boolean balanced(Node node){
+        if(node == null){
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+    
+    private class Node{
+        int value;
+        Node left; 
+        Node right;
+        int height;
+        
+        public Node(int value){
+            this.value = value;
+        }
+        
+        public int getValue(){
+        return value ;
+        }
+        
+    }
+
+    public static void main(String args[]) {
+       int [] arr = {5,27,31,9,2,79,65,89,0,1};
+      //  int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        BST tree = new BST();
+        tree.populate(arr);
+        tree.display();
+        System.out.println(tree.balanced());
+    }
+}
---
+++ src/main/java/Trees/BinaryTree.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package Trees;
+
+import java.util.Scanner;
+
+/**
+ *Date: 09/19/2023
+ * @author parth
+ */
+public class BinaryTree {
+    private Node root;
+    
+    public BinaryTree(){
+        
+    }
+    //insert Elements
+    public void insert(Scanner scanner){
+        System.out.println("Enter the root node value : ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        insert(scanner,root);
+    }
+    private void insert(Scanner scanner, Node node){
+        System.out.println("Do you want to enter left of :" + node.value);
+        boolean left = scanner.nextBoolean();
+        if(left){
+            System.out.println("Enter the value of the left of :" + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            insert(scanner,node.left);
+        }
+        System.out.println("Do you want to enter right of : " + node.value);
+        boolean right = scanner.nextBoolean();
+        if(right){
+            System.out.println("Enter the value of the right of :" + node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            insert(scanner, node.right); 
+        }      
+    }
+    public void display(){
+        display(root," ");
+    }
+
+    private void display(Node node, String indent) {
+        if(node == null){
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");       
+    }
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.value + "-->");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + "--->");
+        inOrder(node.right);
+    }
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + "--->");
+        
+    }
+    
+    
+    private class Node{
+        int value;
+        Node left; 
+        Node right;
+        
+        public Node(int value){
+            this.value = value;
+        }
+    }
+  
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.insert(scanner);
+        tree.display();
+        tree.preOrder();
+        System.out.println("");
+        tree.inOrder();
+        System.out.println("");
+        tree.postOrder();
+        
+        // TODO code application logic here
+    }
+}
