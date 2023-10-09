a5be54633c9a0b6a6737f26df250a9e733db2385
@parthdesai <parth@DESKTOP-3CV400I>
2023-10-09 00:54:33 -0400

    Tree Problems

---
+++ src/main/java/TreesProblems/BinaryTreePaths.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package TreesProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author parth
+ */
+public class BinaryTreePaths {
+      private Node root;
+      public BinaryTreePaths(){
+          
+      }
+      public void insert(Scanner scanner){
+        System.out.println("Enter the root value ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        insert(root,scanner);
+    }
+    private void insert(Node node, Scanner scanner){
+        System.out.println("Do you want to enter value to the left of " + node.value);
+        boolean left = scanner.nextBoolean();
+        if(left){
+            System.out.println("Enter the value to the left of " + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            insert(node.left,scanner);
+        }
+        System.out.println("Do you want to enter value to the right of " + node.value);
+        boolean right = scanner.nextBoolean();
+        if(right){
+            System.out.println("Enter the value to the right of " + node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            insert(node.right,scanner);
+        }
+    }
+    public List<String> binaryPaths(){
+        List<String> answer = new ArrayList<>();
+        binaryPaths(root,"",answer);
+        return answer;
+    }
+    public void binaryPaths(Node node,String str,List<String> answer){
+        if(node == null){
+            return;
+        }
+         str= str + node.value+"->";
+        if(node.left == null && node.right == null){
+            answer.add(str.substring(0,str.length()-2));
+        }
+        binaryPaths(node.left,str,answer);
+        binaryPaths(node.right,str,answer);
+    }
+
+      private class Node{
+        int value;
+        Node left;
+        Node right;
+        
+        public Node(int value){
+            this.value = value;
+        }
+        public Node(int value, Node left, Node right){
+            this.value = value;
+            this.left =left;
+            this.right = right;
+        }
+    }
+    public static void main(String args[]) {
+        BinaryTreePaths tree = new BinaryTreePaths();
+        Scanner scanner = new Scanner(System.in);
+        tree.insert(scanner);
+        List<String> element = tree.binaryPaths();
+        for(String i : element){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/TreesProblems/SumofLeftLeaves.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package TreesProblems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *https://leetcode.com/problems/sum-of-left-leaves/
+ * Date:10/08/2023
+ * @author parth
+ */
+public class SumofLeftLeaves {
+    private Node root;
+    public SumofLeftLeaves(){
+        
+    }
+    public int sumOfLeftLeaves(){
+        return sumofLeftLeaves(root);
+    }
+    private int sumofLeftLeaves(Node node){
+       int sum = 0;
+       List<Integer> answer = new ArrayList<>();
+       if(node == null){
+           return 0;
+       }
+       Queue<Node> queue = new LinkedList<>();
+       queue.offer(node);
+       while(!queue.isEmpty()){
+           Node currentNode = queue.poll();
+           if(currentNode.left!=null){
+               if(currentNode.left.left == null && currentNode.right.right == null){
+                   sum = sum + currentNode.left.value;
+                   queue.offer(currentNode.left);
+               }
+           }
+           if(currentNode.right!=null){
+               queue.offer(currentNode.right);
+           }
+       }
+      
+        return sum;
+       
+    }
+    private int sumofLeftLeavesRecurrsion(Node node){
+        if(node == null){
+            return 0;
+        }
+        if(node.left!=null && node.left.left == null && node.right.right == null){
+            return  node.left.value + sumofLeftLeavesRecurrsion(node.right);
+        }
+        return sumofLeftLeavesRecurrsion(node.left) + sumofLeftLeavesRecurrsion(node.right);
+    }
+    
+    private class Node{
+        int value;
+        Node left;
+        Node right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
