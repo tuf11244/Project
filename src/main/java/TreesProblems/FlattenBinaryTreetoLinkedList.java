f6281e61adeb2506d514558a6a36bb9ec9ef2fa3
@parthdesai <parth@DESKTOP-3CV400I>
2023-10-11 21:20:09 -0400

    Tree Problems

--- src/main/java/TreesProblems/FlattenBinaryTreetoLinkedList.java
+++ src/main/java/TreesProblems/FlattenBinaryTreetoLinkedList.java
@@ -4,6 +4,9 @@
  */
 package TreesProblems;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Date: 09/28/2023
  *https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
@@ -26,16 +29,43 @@ public class FlattenBinaryTreetoLinkedList {
             if(currentNode.left!=null){
             Node temp = currentNode.left;
            while(temp.right!=null){
-               temp = temp.right;
+               temp = temp.right; //Find the right most avalaible Node
            }
            
-           temp.right = currentNode.right;
-           currentNode.right = currentNode.left;
-           currentNode.left = null;
+           temp.right = currentNode.right; //Move all the elements from right to the temp.right
+           currentNode.right = currentNode.left; //Start moving the first elemet from left to right 
+           currentNode.left = null; //Assing currentNdoe.left equal to null
         }
-            currentNode = currentNode.right;
+            currentNode = currentNode.right; //Traverse along the path
     }
+    }    
+    public void flattenBadSolution(){
+        flatten(root);
     }
+    private void flatteBadSolution(Node node){
+        if(node == null){
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node); //Add the root the root node
+    
+        while(!queue.isEmpty()){  //Traverse the entire tree 
+           Node currentNode = queue.poll();  //Take current Node as the root node 
+            if(currentNode.left!=null){  //Store the values in the queue
+                queue.offer(currentNode);
+            }
+            if(currentNode.right!=null){  //Store the values in the queue
+                queue.offer(currentNode);
+
+            }
+            if(!queue.isEmpty()){
+                currentNode.right = queue.peek();
+            }
+            currentNode.left = null;
+        }
+        
+    }
+
 
     private class Node{
         int value;
