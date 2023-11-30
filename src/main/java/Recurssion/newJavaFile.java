d4949e20cc3ac3eb9fe70a9421a0e5012bff83d4
@parthdesai <parth@DESKTOP-3CV400I>
2023-11-29 19:45:49 -0500

    Recursion Practice Question

--- src/main/java/Recurssion/newJavaFile.java
+++ src/main/java/Recurssion/newJavaFile.java
@@ -5,21 +5,61 @@
 package Recurssion;
 import java.util.Scanner;
 
-/**
- *
- * @author parth
- */
+
 public class newJavaFile {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
-        System.out.println("Hello");
-        Scanner console = new Scanner(System.in);
-        System.out.println("Enter any number");
-        int number = console.nextInt();
-        System.out.println(number);
+    public static int findLongestPath(int[][] matrix, int xs, int ys, int xd, int yd) {
+        int[][] visited = new int[matrix.length][matrix[0].length];
+        return dfs(matrix, xs, ys, xd, yd, visited);
+    }
+
+    private static int dfs(int[][] matrix, int currentRow, int currentCol, int destRow, int destCol, int[][] visited) {
+        if (!isValid(matrix, currentRow, currentCol) || matrix[currentRow][currentCol] == 0 || visited[currentRow][currentCol] == 1) {
+            return -1;
+        }
+
+        // Mark the current cell as visited
+        visited[currentRow][currentCol] = 1;
+
+        // Check if the destination is reached
+        if (currentRow == destRow && currentCol == destCol) {
+            return 0;
+        }
+
+        // Explore adjacent cells
+        int up =  1 + dfs(matrix, currentRow - 1, currentCol, destRow, destCol, visited);
+        int down = 1+  dfs(matrix, currentRow + 1, currentCol, destRow, destCol, visited);
+        int left = 1 + dfs(matrix, currentRow, currentCol - 1, destRow, destCol, visited);
+        int right = 1+ dfs(matrix, currentRow, currentCol + 1, destRow, destCol, visited);
+
+        // Reset the visited status (backtrack)
+        visited[currentRow][currentCol] = 0;
+
+        // If any of the paths is impossible, return -1
+        if (up == -1 && down == -1 && left == -1 && right == -1) {
+            return -1;
+        }
+
+        // Return the length of the current path
+        return  Math.max(Math.max(up, down), Math.max(left, right));
+    }
+
+    private static boolean isValid(int[][] matrix, int row, int col) {
+        return row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        int sourceRow = 0, sourceCol = 0;
+        int destRow = 1, destCol = 8;
+
+        int result = findLongestPath(matrix, sourceRow, sourceCol, destRow, destCol);
+
+        System.out.println("The length of the longest path is: " + result);
     }
 }
---
+++ src/main/java/RecurssionPracticeQuestion/LongestPathinMatrix.java
@@ -0,0 +1,61 @@
+package RecurssionPracticeQuestion;
+
+/**
+ * https://www.geeksforgeeks.org/longest-possible-route-in-a-matrix-with-hurdles/
+ * Given an N x M matrix, with a few hurdles(denoted by 0) arbitrarily placed, calculate the length of the longest possible route possible from source(xs,ys) to a destination(xd,yd) within the matrix.
+ * We are allowed to move to only adjacent cells which are not hurdles.
+ * The route cannot contain any diagonal moves and a location once visited in a particular path cannot be visited again.
+ * If it is impossible to reach the destination from the source return -1.
+ * Date: 11/29/2023
+ * @author parth
+ */
+public class LongestPathinMatrix {
+
+    static int finalx;
+    static int finaly;
+
+    public static void main(String args[]) {
+        int[][] board = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        int result = longestPathinMatrix(board, 0, 0, 0, 8);
+        System.out.println(result);
+    }
+
+    public static int longestPathinMatrix(int[][] board, int sourcex, int sourcey, int destinationx, int destinationy) {
+        finalx = destinationx;
+        finaly = destinationy;
+
+        if (board[sourcex][sourcey] == 0 || board[finalx][finaly] == 0) {
+            return -1;
+        }
+        int answer = longestPathinMatrix(board, sourcex, sourcey);
+        return answer;
+    }
+
+    public static boolean isValid(int[][] board, int row, int column) {
+        return row >= 0 && column >= 0 && row < board.length && column < board[0].length && board[row][column] != 0;
+    }
+
+    public static int longestPathinMatrix(int[][] board, int row, int column) {
+        // Base Case
+        if (row == finalx && column == finaly) {
+            return 0;
+        }
+        if (!isValid(board, row, column)) {
+            return -1;
+        }
+        board[row][column] = 0;
+        int right = 1 + longestPathinMatrix(board, row, column + 1);
+        int left = 1 + longestPathinMatrix(board, row, column - 1);
+        int up = 1 + longestPathinMatrix(board, row - 1, column);
+        int down = 1 + longestPathinMatrix(board, row + 1, column);
+
+        board[row][column] = 1;
+
+        return Math.max(Math.max(right, left), Math.max(up, down));
+    }
+}
