09ca3771e584f5ab97b2ab7dc6f9476486d7de40
@parthdesai <parth@DESKTOP-3CV400I>
2023-12-07 22:46:36 -0500

    Recursion Practice Question

---
+++ src/main/java/RecurssionPracticeQuestionPart2/ReachANumber.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package RecurssionPracticeQuestionPart2;
+
+/**
+ *
+ * @author parth
+ */
+public class ReachANumber {
+//https://leetcode.com/problems/reach-a-number/
+        public static void main(String[] args) {
+            System.out.println(reachNumber(1)); 
+            System.out.println(reachNumber(26)); 
+    }
+
+    public static int reachNumber(int target) {
+        target = Math.abs(target);
+        int steps = 0;
+        int currentSum = 0;
+
+        while (currentSum < target || (currentSum - target) % 2 != 0) {
+           steps = steps + 1;
+        currentSum = currentSum + steps;
+        }
+
+        return steps;
+    }
+}
+
