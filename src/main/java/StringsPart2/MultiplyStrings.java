74448e453fe287565bb1cab800bac20f850123b8
@parthdesai <parth@DESKTOP-3CV400I>
2023-12-22 23:47:38 -0500

    String Questions

--- src/main/java/StringsPart2/MultiplyStrings.java
+++ src/main/java/StringsPart2/MultiplyStrings.java
@@ -4,6 +4,8 @@
  */
 package StringsPart2;
 
+import java.util.ArrayList;
+
 /**
  *https://leetcode.com/problems/multiply-strings/
  * Date: 12/20/2023
@@ -15,7 +17,8 @@ public class MultiplyStrings {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        System.out.println(multiply("123","456"));
+       // System.out.println(multiply("123","456"));
+        System.out.println(add("146","9854"));
     }
     public static String multiply(String num1, String num2){
         if(num1.equals("0") || num2.equals("0")){
@@ -66,4 +69,65 @@ public class MultiplyStrings {
 
         return answer.toString();
     }
-}
+    
+    //Add two Strings 
+    public static String add(String num1, String num2){
+        //Base Cases 
+        //Case 1 : If both the Strings are 0
+        if(num1.equals("0") && num2.equals("0")){
+            return "0";
+        }
+        //Case 2 and 3 : if one of the nums is 0
+        if(num1.equals("0") && !num2.equals("0")){
+            return num2;
+        }
+        if(num2.equals("0") && !num1.equals("0")){
+            return num1;
+        }
+        
+        String longest = num1.length() > num2.length() ? num1 : num2;
+        String shortest = num1.equals(longest) ? num2 : num1;
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        int k = longest.length() - shortest.length();
+        
+        while(k > 0){
+            shortest = "0" + shortest;
+            k--;
+        }
+        System.out.println(longest);
+        System.out.println(shortest);
+        
+        int carry = 0;
+       //At this point all the Strings are equal
+        for(int i = shortest.length() -1; i >= 0; i--){
+            int digit1 = longest.charAt(i) - '0';
+            int digit2 = shortest.charAt(i) - '0';
+            
+            int sum = digit1 + digit2 + carry;
+            carry = sum/10;
+            
+            if(carry > 0){
+                list.add(sum%10);
+            }else{
+                list.add(sum);
+            }
+        }
+        if(carry > 0){
+            list.add(carry);
+        }
+        
+        String answer = "";
+        
+        System.out.println(list);
+        
+        for(int j = list.size()-1; j >= 0; j--){
+            answer = answer + list.get(j);
+        }
+        return answer;
+        
+        }
+        
+    }
+    
+
