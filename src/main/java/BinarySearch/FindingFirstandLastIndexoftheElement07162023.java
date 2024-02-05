776b31b0af678009db1e7c7a6801ac3b6de8e371
@parthdesai <parth@DESKTOP-3CV400I>
2024-02-05 16:45:03 -0500

    Binary Search Revision

--- src/main/java/BinarySearch/FindingFirstandLastIndexoftheElement07162023.java
+++ src/main/java/BinarySearch/FindingFirstandLastIndexoftheElement07162023.java
@@ -5,7 +5,11 @@
 package BinarySearch;
 import java.util.Arrays;
 /**
- *
+ *https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ * https://www.geeksforgeeks.org/count-1s-sorted-binary-array
+ * 
+ * Same question logic used for find the count of given element in binary sorted array
+ * first occurrence - last occurence + 1;
  * @author parth
  */
 
@@ -16,48 +20,95 @@ public class FindingFirstandLastIndexoftheElement07162023 {
      */
    
     public static void main(String[] args){
-		System.out.println("Hello World");
-		int[]arr ={5,7,7,8,8,8,10};
-		System.out.println(Arrays.toString(firstandlastelementsearch(arr,8)));
+	System.out.println("Hello World");
+	int[]arr ={5,7,7,8,8,8,10};
+        int[] nums = {1,1,0,0};
+	//System.out.println(Arrays.toString(searchRange(arr,8)));
+        //System.out.println(countNumberofOnes(nums));
+        System.out.println(countNumberofOnesinDecreasingArray(nums));
                 
 	}
-	
-    public static int[] firstandlastelementsearch(int[]arr, int target){
-	    int[] answer = {-1,-1};
-	    int start = search(arr,target, true);
-	    int end = search(arr,target,false);	    
-	    answer[0] = start;
-	    answer[1] = end;
-	    return answer;
-	}
-	
-    public static int search(int[]arr, int target, boolean findStartIndex){
-	    int answer = -1;
-	    int start = 0;
-	    int end = arr.length -1;
-	    while(start <= end){
-	        int middle = (start+end)/2;
-	        if(arr[middle] < target){
-	            start = middle + 1;
-                    
-	        }
-	        else if(arr[middle] > target){
-	            end = middle - 1;
-	        }
-	        else{
-	            //Potential Answer is found
-	            answer = middle;
-	            if(findStartIndex){
-	                start = middle + 1; 
-	            }
-	            else{
-	                end = middle - 1;                                          
-	            }
-	        }
-	    
-	}     
-            
-	return answer;
-}
+    public static int[] searchRange(int[] nums, int target) {
+        int start = helper(nums,target);
+        int end =  searchRangeHelper(nums,target);
+
+        return new int[]{start,end};
+    }
+    public static int helper(int[] nums, int target){
+        int low = 0;
+        int end = nums.length -1;
+        int result = -1;
+        while(low <= end){
+            int mid = low + (end - low)/2;
+            if(nums[mid] == target){
+                result = mid;
+                end = mid - 1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                end = mid - 1;
+            }            
+        }
+        return result;
+    }
+    public static int searchRangeHelper(int[] nums, int target){
+        int low = 0;
+        int end = nums.length -1;
+        int result = -1;
+        while(low <= end){
+            int mid = low + (end - low)/2;
+            if(nums[mid] == target){
+                result = mid;
+                low = mid + 1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                end = mid - 1;
+            }            
+        }
+        return result;
+    }
+    
+    
+    //Question Find the number of ones, count number ones, its the same question we just have find the first occurence of 1 
+    //int[] arr = {0,0,1,1,1}
+    public static int countNumberofOnes(int[] arr){
+        int low = 0;
+        int end = arr.length - 1;
+        int result = -1;
+        while(low <= end){
+           int mid = low + (end - low)/2;
+           if(arr[mid] == 1){
+               result = mid;
+               end = mid - 1;
+           }else if(arr[mid] == 0){
+               low = mid + 1;
+           }
+        }
+        //Below statement states that if result is -1 that means there are no ones so return 0 
+        //If not than return the arr.length - first occurence 
+        //for example arr = {0,0,0,0,1,1}
+        //return 2 which is arr.length - 4, 4 being first occurence of 1
+        return result == -1 ? 0 : arr.length - result;
+    }
+    
+    //Decreasing Array 
+    //int[] arr = {1,1,0,0,0]
+    public static int countNumberofOnesinDecreasingArray(int[] arr){
+        int low = 0;
+        int end = arr.length -1;
+        int result = -1;
+        while(low <= end){
+            int mid = low + (end - low)/2;
+            if(arr[mid] == 1){
+                result = mid;
+                low = mid + 1;
+            }else if (arr[mid] == 0){
+                end = mid - 1;
+            }
+        }
+        return result + 1;
+    }
+    
 }
    
