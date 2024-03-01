48b329b5a6e0ce9e1c96831998cb7825ad4ec2b5
@parthdesai <parth@DESKTOP-3CV400I>
2024-02-29 23:39:56 -0500

    Heaps

--- src/main/java/HeapTopKPatternQuestions/TopKFrequentWords.java
+++ src/main/java/HeapTopKPatternQuestions/TopKFrequentWords.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package HeapsQuestions;
+package HeapTopKPatternQuestions;
 
+import HeapTopKPatternQuestions.Pair;
 import java.util.ArrayList;
 
 import java.util.HashMap;
