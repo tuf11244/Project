abe446858c406a422ade4c505b9d80ec58701a52
@parthdesai <parth@DESKTOP-3CV400I>
2024-03-01 00:20:29 -0500

    Heaps

---
+++ src/main/java/HeapTopKPatternQuestions/SortCharactersbyFrequency.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package HeapTopKPatternQuestions;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Collections;
+/**
+ *Date:02/28/2024
+ * https://leetcode.com/problems/sort-characters-by-frequency/
+ * @author parth
+ */
+public class SortCharactersbyFrequency {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println(frequencySort("tree"));
+    }
+    public static String frequencySort(String s) {
+        HashMap<Character,Integer>  hm = new HashMap<>();
+        //Creater a Frequecy Map;
+        
+        for(int i = 0; i < s.length();i++){
+            if(hm.containsKey(s.charAt(i))){
+                int oldFrequency = hm.get(s.charAt(i));
+                int newFrequency = oldFrequency + 1;
+                hm.put(s.charAt(i), newFrequency);
+            }else{
+                hm.put(s.charAt(i),1);
+            }            
+        }
+        
+        PriorityQueue<Pair2> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        //Iterate over our Hash Map 
+        for(Character c : hm.keySet()){
+            pq.add(new Pair2(c,hm.get(c)));
+        }
+        System.out.println(pq);
+        
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            Pair2 remove = pq.poll();
+            int frequency = remove.frequency;
+            while(frequency!=0){
+                sb.append(remove.c);
+                frequency--;
+            }
+        }
+        return sb.toString();
+        
+    }
+}
+class Pair2 implements Comparable<Pair2>{
+  char c;
+  int frequency;
+  
+  Pair2(Character c, int frequency){
+      this.c = c;
+      this.frequency = frequency;
+  }
+
+    @Override
+    public int compareTo(Pair2 o) {
+        return this.frequency - o.frequency;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" + "c=" + c + ", frequency=" + frequency + '}';
+    } 
+  
+}
+
