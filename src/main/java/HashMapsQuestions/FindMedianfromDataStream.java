2b8747604056c45011876e0b6d13049e1182c51e
@parthdesai <parth@DESKTOP-3CV400I>
2024-03-03 02:21:35 -0500

    HashMaps

---
+++ src/main/java/HashMapsQuestions/FindMedianfromDataStream.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package HashMapsQuestions;
+
+import java.util.PriorityQueue;
+
+/**
+ *Date: 03/02/2024
+ * https://leetcode.com/problems/find-median-from-data-stream/
+ * @author parth
+ */
+public class FindMedianfromDataStream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
+
+class MedianFinder {
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    }
+
+    public void addNum(int num) {
+        maxHeap.add(num);
+        minHeap.add(maxHeap.poll());
+
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (double) (maxHeap.peek() + minHeap.peek()) / 2;
+        } else {
+            return (double) maxHeap.peek();
+        }
+    }
+}
+
