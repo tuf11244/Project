90eb77f09ef1cb65d66b81dd6835788203131fd8
@parthdesai <parth@DESKTOP-3CV400I>
2024-04-05 00:29:31 -0400

    heaps

---
+++ src/main/java/HeapsQuestions/EmployeeFreeTime.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package HeapsQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ *DateL 04/04/2024
+ *https://leetcode.com/problems/employee-free-time/
+ * @author parth
+ */
+public class EmployeeFreeTime {
+
+    /**
+     * It was 12:25 AM on 04/05/2024
+     * Started working on this problem at 10: 30
+     * Still can't believe I did it by myself 
+     * and it is classified as leetcode hard :)
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int[][] schedule = {
+            {1,3}, 
+            {8,10}, 
+            {15,20},
+        };
+        
+        List<List<Integer>> answer = employeeFreeTime(schedule);
+        
+        for(List<Integer> x : answer){
+            System.out.println(x);
+        }
+          
+    }
+    
+    public static List<List<Integer>> employeeFreeTime(int[][] schedule){
+        List<List<Integer>> answer = new ArrayList<>();
+        PriorityQueue<Schedule> pq = new PriorityQueue<>();   //Min heap 
+        if(schedule.length == 1){
+            return answer;
+        }
+        //Fill up our priority Queue 
+        for(int i = 0; i < schedule.length;i++){
+            int startTime = schedule[i][0];
+            int endTime = schedule[i][1];
+            pq.add(new Schedule(startTime,endTime));
+        }
+        System.out.println(pq);
+        int newCurrentEndTime = 0;
+        while(!pq.isEmpty()){
+            Schedule current = pq.poll();
+            int endT = (newCurrentEndTime == 0) ? current.endTime : newCurrentEndTime;
+            if(!pq.isEmpty() && endT < pq.peek().startTime){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(endT);
+                temp.add(pq.peek().startTime);
+                answer.add(temp);
+                
+                newCurrentEndTime = pq.peek().endTime;
+            }
+            else{
+                if(!pq.isEmpty() && endT < pq.peek().endTime){
+                    newCurrentEndTime = pq.peek().endTime;
+                }else{
+                    newCurrentEndTime = current.endTime;
+                }
+                
+            }           
+        }
+
+        return answer;
+        
+    }
+}
+
+class Schedule implements Comparable<Schedule>{
+    int startTime;
+    int endTime;
+    int difference;
+    
+    public Schedule(int startTime, int endTime){
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.difference = this.endTime - this.startTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" + "startTime=" + startTime + ", endTime=" + endTime + ", difference=" + difference + '}';
+    }
+    
+    @Override
+    public int compareTo(Schedule o) {
+        if(this.startTime != o.startTime){
+            return this.startTime - o.startTime;
+        }
+        return this.difference - o.difference; //when the startTime are equal
+    }
+            
+}
---
+++ src/main/java/HeapsQuestions/TaskScheduler.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package HeapsQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ *Date: 04/04/2024
+ *https://leetcode.com/problems/task-scheduler
+ * @author parth
+ */
+public class TaskScheduler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        
+        char[] tasks = {'A','A','A', 'B','B','B'};
+       int answer = leastInterval(tasks,2);
+        System.out.println(answer);
+    }
+    
+    public static int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        String s = new String(tasks);
+        for(int i = 0; i < s.length();i++){
+            map[s.charAt(i)-'A']++;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+       for(int i = 0; i < map.length;i++){
+           if(map[i] > 0){
+               pq.add(map[i]);
+           }
+       }
+        int time = 0;
+        // Process tasks until the heap is empty
+        while (!pq.isEmpty()) {
+            int cycle = n + 1;
+            List<Integer> list = new ArrayList<>();
+            int taskCount = 0;
+            // Execute tasks in each cycle
+            while (cycle > 0 && !pq.isEmpty()) {
+                int currentFreq = pq.poll();
+                if (currentFreq > 1) {
+                    list.add(currentFreq - 1);
+                }
+                taskCount++;
+                cycle--;
+            }
+            // Restore updated frequencies to the heap
+            for (int updatedFreq : list) {
+                pq.offer(updatedFreq);
+            }
+                // Add time for the completed cycle
+            if (pq.isEmpty()) {
+                time += taskCount;
+            } else {
+                 time += n + 1;
+            }     
+        }
+        return time;
+    }
+              
+} 
+    
+   
