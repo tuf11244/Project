79ca5a3fa3b5548e173c3a9e8a082c0db9a4bf2d
@parthdesai <parth@DESKTOP-3CV400I>
2024-04-20 19:02:52 -0400

    Advanced Trees Questions

--- src/main/java/TreesProblems/ConstructBTfrompreOrderandinOrder.java
+++ src/main/java/TreesProblems/ConstructBTfrompreOrderandinOrder.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 /**
  * Date:09/29/2023
  *https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * 04/20/2024 - https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
  * @author parth
  */
 public class ConstructBTfrompreOrderandinOrder {
@@ -54,3 +55,39 @@ public class ConstructBTfrompreOrderandinOrder {
         // TODO code application logic here
     }
 }
+
+
+//https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ class Node{
+        int value;
+        Node left;
+        Node right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+   
+class Solution {
+    public Node buildTree(int[] inorder, int[] postorder) {
+        if(postorder.length == 0 || inorder.length == 0){
+            return null;
+        }
+        int rootValue = postorder[postorder.length-1];
+        int index = 0;
+        for(int i = 0; i < inorder.length;i++){
+            if(inorder[i] == rootValue){
+                index = i;
+                break;
+            }
+        }
+
+        //Create the node for the root value 
+        Node node = new Node(rootValue);
+
+        //Recursion Call
+        node.left = buildTree(Arrays.copyOfRange(inorder,0,index),Arrays.copyOfRange(postorder,0,index));
+        node.right = buildTree(Arrays.copyOfRange(inorder,index+1,inorder.length),Arrays.copyOfRange(postorder,index,postorder.length-1));
+        return node;
+    }
+}
+
