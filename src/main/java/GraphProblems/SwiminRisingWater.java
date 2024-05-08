2782d9aba569035c91bc12d8bce78e60c71598f7
@parthdesai <parth@DESKTOP-3CV400I>
2024-05-08 19:29:05 -0400

    Graphs

--- src/main/java/GraphProblems/SwiminRisingWater.java
+++ src/main/java/GraphProblems/SwiminRisingWater.java
@@ -1,3 +1,5 @@
+package GraphProblems;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
